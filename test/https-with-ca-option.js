4a7fb89d3fd2e8477a854946e36ea129d18f1ceb
Thomas Reggi <reggi@github.com>
2024-05-21 16:06:22 -0400

    chore: tests NODE_TLS_REJECT_UNAUTHORIZED (#149)

--- test/https-with-ca-option.js
+++ test/https-with-ca-option.js
@@ -28,22 +28,42 @@ t.before(() => new Promise((res) => {
     s.end(`${q.method} ${q.url}`)
   }).listen(port, res)
 }))
+
 t.teardown(() => server.close())
 
+const failure = {
+  name: 'FetchError',
+  message: `request to ${base}hello failed, reason: unable to verify the first certificate`,
+  code: 'UNABLE_TO_VERIFY_LEAF_SIGNATURE',
+  errno: 'UNABLE_TO_VERIFY_LEAF_SIGNATURE',
+  type: 'system',
+}
+
 // this test will fail after Jan 30 23:23:26 2025 GMT
-t.test('make https request without ca, should fail', t =>
-  t.rejects(fetch(`${base}hello`), {
-    name: 'FetchError',
-    message: `request to ${base}hello failed, reason: unable to verify the first certificate`,
-    code: 'UNABLE_TO_VERIFY_LEAF_SIGNATURE',
-    errno: 'UNABLE_TO_VERIFY_LEAF_SIGNATURE',
-    type: 'system',
-  }))
+t.test('make https request without ca, should fail', async t => {
+  await t.rejects(fetch(`${base}hello`), failure)
+})
 
-t.test('make https request with rejectUnauthorized:false, succeeds', async t =>
-  t.equal(await (await fetch(`${base}hello`, { rejectUnauthorized: false })).text(),
-    'GET /hello'))
+t.test('make https request with NODE_TLS_REJECT_UNAUTHORIZED set to 1, should fail', async t => {
+  t.teardown(() => delete process.env.NODE_TLS_REJECT_UNAUTHORIZED)
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '1'
+  await t.rejects(fetch(`${base}hello`), failure)
+})
 
-t.test('make https request with ca, succeeds', async t =>
-  t.equal(await (await fetch(`${base}hello`, { ca })).text(),
-    'GET /hello'))
+t.test('make https request with rejectUnauthorized:true, should fail', async t => {
+  await t.rejects(fetch(`${base}hello`, { rejectUnauthorized: true }), failure)
+})
+
+t.test('make https request with NODE_TLS_REJECT_UNAUTHORIZED set to 0, succeeds', async t => {
+  t.teardown(() => delete process.env.NODE_TLS_REJECT_UNAUTHORIZED)
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+  t.equal(await (await fetch(`${base}hello`)).text(), 'GET /hello')
+})
+
+t.test('make https request with rejectUnauthorized:false, succeeds', async t => {
+  t.equal(await (await fetch(`${base}hello`, { rejectUnauthorized: false })).text(), 'GET /hello')
+})
+
+t.test('make https request with ca, succeeds', async t => {
+  t.equal(await (await fetch(`${base}hello`, { ca })).text(), 'GET /hello')
+})
